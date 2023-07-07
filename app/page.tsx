7c70e1f62ea75738b1c1f0fb96ec79146a91d76b
alexanderborisovvrn <alexanderborisovvrn@gmail.com>
2023-07-07 10:55:25 +0300

    feat:responsive home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className=" min-h-screen flex flex-row">
-      <div className="flex justify-center items-center flex-col text-center min-h-screen w-1/2 bg-primary border-r border-slate-800 p-[24px]">
-        <h1 className='text-9xl font-bold  '>The most <br />epic products</h1>
-        <p className='text-5xl w-3/4 my-10'>All The most epic things on the internet in one place</p>
-        <Link href='/products' className='flex justify-center items-center rounded-md text-left text-2xl hover:opacity-90 w-1/2 h-20 text-yellow-50 uppercase  bg-slate-950'>Explore Products</Link>
-      </div>
-      <div className="flex justify-center items-center min-h-screen  w-1/2 bg-chrome border-l border-slate-800">
-        <div className='relative w-1/2 h-1/2'>
-          <Image src='/assets/brain3.png' placeholder='empty' alt='brain' fill={true} />
+    <main className=" min-h-screen flex flex-col lg:flex-row">
+      <section className="flex justify-center items-center flex-col text-center min-h-screen w-full  lg:w-1/2 bg-primary border-r border-slate-800 p-[24px]">
+        <h1 className='text-7xl  md:text-8xl xl:text-9xl font-bold  '>The most <br />epic products</h1>
+        <p className='text-2xl  lg:text-5xl w-3/4 my-10'>All The most epic things on the internet in one place</p>
+        <Link href='/products' className='flex justify-center items-center rounded-md text-left text-2xl hover:opacity-90 w-1/2 h-20 text-yellow-50 uppercase  bg-slate-950'><button>Explore Products</button></Link>
+      </section>
+      <section className="flex justify-center items-center lg:min-h-screen  w-full lg:w-1/2 bg-chrome border-l border-slate-800">
+        <div className='relative hidden lg:block md:w-1/3 lg:w-1/2 lg:h-1/3 xl:h-1/2'>
+          <Image src='/assets/brain3.png' placeholder='empty' alt='brain' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
         </div>
-      </div>
+      </section>
     </main>
   )
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,22 @@
+import {supabase} from '../../supabase/supabese'
 
-const Products = () => {
+//@ts-ignore
+const Products = async({products}) => {
+
+console.log(products)
   return (
       <section className=" border-black border-x-0">
-        <h1 className="flex items-center justify-center bg-secondary h-64 xs:text-5xl sm:text-7xl lg:text-9xl">The latest Products</h1>
-
+        <h1 className="flex items-center justify-center bg-secondary h-64 text-4xl sm:text-7xl lg:text-9xl">The latest Products</h1>
+        <div className="bg-white">
+          <ul></ul>
+        </div>
       </section>
   )
 }
-
+export const getStaticProps = async () => {
+  let { data: products, error } = await supabase
+  .from('product')
+  .select('*')
+  return { props: { products } }
+}
 export default Products
