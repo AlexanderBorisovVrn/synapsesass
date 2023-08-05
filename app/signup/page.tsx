6908b4a1c05af0cfc128b529561e24dda7cf1ffa
alexanderborisovvrn <alexanderborisovvrn@gmail.com>
2023-08-05 11:34:24 +0300

    feat:add signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,23 +2,48 @@
 import Background from '@/components/Background'
 import FormInput from '@/components/FormInput'
 import Inner from '@/components/Inner'
-import Link from 'next/link'
-import React from 'react'
+import React, { ChangeEvent, useState, useCallback, useEffect } from 'react'
+import { supabase } from '@/supabase/supabese'
+
 
 const SignUp = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const emailHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail( e.target.value )
+  }, [])
+  const passwordHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setPassword( e.target.value )
+  }, []);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(email,password)
+    let { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password
+    })
+    if(error){
+      console.log(error.message)
+    }
+    console.log(data)
+  }
+
+
   return (
     <Background>
       <Inner>
-        <form >
-          <FormInput type='email' id='email' placeholder='jornlee@example.com' handler={() => { }}>
+        <form onSubmit={onSubmit}>
+          <FormInput type='email' id='email' placeholder='jornlee@example.com' handler={emailHandler}>
             Email
           </FormInput>
-          <FormInput type='password' id='password' handler={() => { }}>
+          <FormInput type='password'  id='password' handler={passwordHandler}>
             Password
           </FormInput>
           <button type='submit' className='flex w-full h-10 md:h-12 lg:h-16 bg-blue my-10 justify-center items-center rounded-md text-white'>SignUp</button>
         </form>
-     
+
       </Inner>
     </Background>
   )
--- supabase/supabese.js
+++ supabase/supabese.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://azevtqtkappzxslxcque.supabase.co'
 
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6ZXZ0cXRrYXBwenhzbHhjcXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0Nzg2NDEsImV4cCI6MjAwNDA1NDY0MX0.oOEZDlkVWkCcf88fX8326Z-2l7tUGChFTCnT1e6XVMk'
 export const supabase = createClient(supabaseUrl, supabaseKey,{auth:{persistSession: false}})
