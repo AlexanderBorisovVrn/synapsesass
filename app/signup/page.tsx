f94e2a34b9ca48eef9b1d00e2b78e6d6d54c11f1
alexanderborisovvrn <alexanderborisovvrn@gmail.com>
2023-08-05 12:25:01 +0300

    feat:add error handle for sign up

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,42 +3,48 @@ import Background from '@/components/Background'
 import FormInput from '@/components/FormInput'
 import Inner from '@/components/Inner'
 import React, { ChangeEvent, useState, useCallback, useEffect } from 'react'
-import { supabase } from '@/supabase/supabese'
+import { signUp } from '@/supabase/supabese'
 
 
 const SignUp = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('')
 
   const emailHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setEmail( e.target.value )
+    setEmail(e.target.value)
   }, [])
   const passwordHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setPassword( e.target.value )
+    setPassword(e.target.value)
   }, []);
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(email,password)
-    let { data, error } = await supabase.auth.signUp({
-      email: email,
-      password: password
-    })
-    if(error){
-      console.log(error.message)
+    setErrorMessage('')
+    try {
+
+      const { data, error } = await signUp(email, password);
+      if (error) {
+        setErrorMessage(error.message)
+      }
+      console.log(data)
+      localStorage.setItem('user', JSON.stringify(data));
+
+    } catch (error) {
+      throw error
     }
-    console.log(data)
   }
 
 
   return (
     <Background>
       <Inner>
+        {errorMessage ? <p className='mb-4 text-red-700'>{errorMessage}</p> : null}
         <form onSubmit={onSubmit}>
           <FormInput type='email' id='email' placeholder='jornlee@example.com' handler={emailHandler}>
             Email
           </FormInput>
-          <FormInput type='password'  id='password' handler={passwordHandler}>
+          <FormInput type='password' id='password' handler={passwordHandler}>
             Password
           </FormInput>
           <button type='submit' className='flex w-full h-10 md:h-12 lg:h-16 bg-blue my-10 justify-center items-center rounded-md text-white'>SignUp</button>
--- supabase/supabese.js
+++ supabase/supabese.js
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://azevtqtkappzxslxcque.supabase.co'
 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6ZXZ0cXRrYXBwenhzbHhjcXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0Nzg2NDEsImV4cCI6MjAwNDA1NDY0MX0.oOEZDlkVWkCcf88fX8326Z-2l7tUGChFTCnT1e6XVMk'
-export const supabase = createClient(supabaseUrl, supabaseKey,{auth:{persistSession: false}})
+export const supabase = createClient(supabaseUrl, supabaseKey,{auth:{persistSession: false}});
+
+export async function signUp (email,password){
+    let { data, error } = await supabase.auth.signUp({
+        email,
+        password
+      })
+    return {data,error}
+}
